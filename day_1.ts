5e495308600eaa6c6c75dc133ff555c273a43fad
nomit <timon@thenamedoesnotmatter.com>
2022-12-01 10:39:43 +0100

    day 1

---
+++ day_1.ts
@@ -0,0 +1,36 @@
+import * as fs from "fs";
+
+fs.readFile(
+  "aoc_1.txt",
+  "utf8",
+  (err: NodeJS.ErrnoException | null, data: string) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    const lines: Array<string> = data.split(/\r?\n/);
+    let count = 0;
+
+    const arrayWithCounts = lines.flatMap((value) => {
+      if (value !== "") {
+        count += parseInt(value);
+      } else {
+        const currentCount = count;
+        count = 0;
+        return currentCount;
+      }
+      return [];
+    });
+
+    // day 1:
+    console.log(Math.max(...arrayWithCounts));
+
+    // day 2:
+    const topThree = arrayWithCounts
+      .sort((a, b) => (a < b ? 1 : a > b ? -1 : 0))
+      .slice(0, 3)
+      .reduce((previousValue, currentValue) => previousValue + currentValue);
+
+    console.log(topThree);
+  }
+);
